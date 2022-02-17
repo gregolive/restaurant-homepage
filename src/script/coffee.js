101ff4ebf180476a07f756f48f2863ab2d69fe18
Gregory Olive <gregoryolive@outlook.com>
2022-02-17 22:53:12 +0900

    Update grid and background

--- src/script/coffee.js
+++ src/script/coffee.js
@@ -2,19 +2,34 @@ import buildHeader from './header.js';
 import buildFooter from './footer.js';
 
 import Ethiopia from '../img/Ethiopia.jpg';
+import Kenya from '../img/Kenya.jpg';
+import Colombia from '../img/Colombia.jpg';
+import Brazil from '../img/Brazil.jpg';
+import ElSalvador from '../img/El-Salvador.jpg';
+import Guatemala from '../img/Guatemala.jpg';
+
+function Coffee(name, country, price, image) { 
+  this.name = name; 
+  this.country = country; 
+  this.price = price;
+  this.image = image;
+}
 
 const buildCoffeeGrid = () => {
   const coffeeGrid = document.createElement('div'),
         coffees = [
-          {name: 'YIRGACHEFFE', country: 'Ethopia', price: '980', image: Ethiopia},
-          {name: 'YIRGACHEFFE', country: 'Ethopia', price: '980', image: Ethiopia},
-          {name: 'YIRGACHEFFE', country: 'Ethopia', price: '980', image: Ethiopia}
+          new Coffee('YIRGACHEFFE', 'Ethopia', '1,160', Ethiopia),
+          new Coffee('PB KAMWANGI', 'Kenya', '980', Kenya),
+          new Coffee('FRUTA MERCADAO', 'Brazil', '950', Brazil),
+          new Coffee('HUILA', 'Colombia', '890', Colombia),
+          new Coffee('DON JAMIE', 'El Salvador', '1,020', ElSalvador),
+          new Coffee('EL INJERTO BOURBON', 'Guatemala', '1,100', Guatemala)
         ];
 
   coffeeGrid.className = 'coffee-grid';
   
   coffees.forEach(coffee => {
-    const article = document.createElement('article'),
+    const gridItem = document.createElement('a'),
           title = document.createElement('h2'),
           description = document.createElement('p'),
           img = document.createElement('img');
@@ -24,11 +39,12 @@ const buildCoffeeGrid = () => {
     img.src = coffee.image;
     img.alt = `Coffee beans from ${coffee.country}`;
 
-    article.appendChild(title);
-    article.appendChild(description);
-    article.appendChild(img);
+    gridItem.className = 'grid-item';
+    gridItem.appendChild(title);
+    gridItem.appendChild(description);
+    gridItem.appendChild(img);
 
-    coffeeGrid.appendChild(article);
+    coffeeGrid.appendChild(gridItem);
   })
 
   return coffeeGrid;
